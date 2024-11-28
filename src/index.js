bceed58d6d6a10558642f9c7f7d23981977761f2
Arvin Garcia <arvin.garcia.dev@gmail.com>
2024-11-28 16:56:04 +0800

    Integrate add note button in header
    Add note button currently displays modal but still no form inside.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const sidebar = document.querySelector('.sidebar')
 const overlay = document.querySelector('.overlay');
 const xBtn = document.querySelector('.x-btn')
 
+const addNoteBtn = document.querySelector('.add-note-btn')
+const addNoteDialog = document.querySelector('.add-note-dialog')
+const addNoteForm = document.getElementById('add-note-form')
+
 function toggleSidebar() {
   sidebar.classList.toggle('active')
   overlay.classList.toggle('active')
@@ -13,3 +17,20 @@ function toggleSidebar() {
 hamburgerMenu.addEventListener('click', toggleSidebar)
 overlay.addEventListener('click', toggleSidebar)
 xBtn.addEventListener('click', toggleSidebar)
+
+addNoteBtn.addEventListener('click', () => {
+  const rect = addNoteBtn.getBoundingClientRect();
+
+  addNoteDialog.style.left = `${rect.left - 230}px`;
+  addNoteDialog.style.top = `${rect.bottom}px`;
+
+  addNoteDialog.show();
+});
+
+document.addEventListener('click', (event) => {
+  if (!addNoteDialog.contains(event.target) && !addNoteBtn.contains(event.target)) {
+    addNoteDialog.close();
+  }
+});
+
+
