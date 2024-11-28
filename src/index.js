75c5b7d3d6ec277f6c22ae059f2de58c02f7d773
Arvin Garcia <arvin.garcia.dev@gmail.com>
2024-11-28 12:49:31 +0800

    Add mobile sidebar functionality
    Shows the sidebar when clicking the hamburger menu. Need to further make
    sidebar appear by default on a larger viewport and add JavaScript
    functionalities.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles.css"
 
-console.log('Hello World')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const sidebar = document.querySelector('.sidebar')
+const overlay = document.querySelector('.overlay');
+const xBtn = document.querySelector('.x-btn')
+
+function toggleSidebar() {
+  sidebar.classList.toggle('active')
+  overlay.classList.toggle('active')
+}
+
+hamburgerMenu.addEventListener('click', toggleSidebar)
+overlay.addEventListener('click', toggleSidebar)
+xBtn.addEventListener('click', toggleSidebar)
